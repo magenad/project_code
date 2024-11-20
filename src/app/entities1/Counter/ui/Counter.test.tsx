309e7906335d428919f92671ec96cd94ddc3e723
sadmin <wolf.krsk@gmail.com>
2024-11-20 12:15:26 +0700

    30 Redux fix 4

---
+++ src/app/entities1/Counter/ui/Counter.test.tsx
@@ -0,0 +1,22 @@
+import { screen } from '@testing-library/react';
+import { componentRender } from 'shared/lib/tests/componentRender/componentRender';
+import { Counter } from 'app/entities1/Counter';
+import userEvent from '@testing-library/user-event';
+
+describe('Counter', function () {
+    test('Should be able to create a button', function () {
+        componentRender(<Counter />,{ initialState:{ counter:{ value:10 } } });
+        expect(screen.getByTestId('value-title')).toHaveTextContent('10');
+    });
+    test('increment', async function () {
+        componentRender(<Counter />,{ initialState:{ counter:{ value:10 } } });
+        await userEvent.click(screen.getByTestId('increment-btn'));
+        expect(screen.getByTestId('value-title')).toHaveTextContent('11');
+    });
+    test('decrement', async function () {
+        componentRender(<Counter />,{ initialState:{ counter:{ value:10 } } });
+        await userEvent.click(screen.getByTestId('decrement-btn'));
+        expect(screen.getByTestId('value-title')).toHaveTextContent('9');
+    });
+  
+});
