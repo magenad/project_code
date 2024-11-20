ff75693a46f2ff24dd7ae94c409a0e0a53162a95
sadmin <wolf.krsk@gmail.com>
2024-11-20 12:23:03 +0700

    30 Redux fix 5-

--- src/index.tsx
+++ src/index.tsx
@@ -6,18 +6,17 @@ import { ThemeProvider } from 'app/provider/ThemeProvider';
 import 'shared/config/i18n/i18n';
 import { ErrorBoundary } from 'app/provider/ErrorBoundary';
 import 'app/styles/index.scss';
-import { StoreProvider } from 'app/provider/StoreProvider';
 
 render(
-    <StoreProvider>
-        <BrowserRouter>
-            <ErrorBoundary>
-                <ThemeProvider>
-                    <App/>
-                </ThemeProvider>
-            </ErrorBoundary>
-        </BrowserRouter>
-    </StoreProvider>
+   
+    <BrowserRouter>
+        <ErrorBoundary>
+            <ThemeProvider>
+                <App/>
+            </ThemeProvider>
+        </ErrorBoundary>
+    </BrowserRouter>
+   
     ,
     document.getElementById('root')
 );
