1a3c3ffd50e0046ef83abea964d7a0222c488719
sadmin <wolf.krsk@gmail.com>
2025-04-22 20:01:35 +0700

    51 Comment entities, Entities adapter

---
+++ src/entities/Comment/ui/CommentList/CommentList.tsx
@@ -0,0 +1,32 @@
+import { classNames } from 'shared/lib/classNames/classNames';
+import cls from './CommentList.module.scss';
+import { memo } from 'react';
+import { Comment } from '../../model/types/comment';
+import { Text } from 'shared/ui/Text/Text';
+import { useTranslation } from 'react-i18next';
+import { CommentCard } from '../CommentCard/CommentCard';
+
+interface CommentListProps {
+    className?: string;
+    comments?: Comment[];
+    isLoading?: boolean;
+}
+
+export const CommentList = memo((props: CommentListProps) => {
+    const { className,isLoading,comments } = props;
+    const { t } = useTranslation();
+    return (
+        <div className={classNames(cls.CommentList, {}, [className])}>
+            {comments?.length
+                ? comments.map(comment=>(
+                    <CommentCard
+                        key={comment.id}
+                        comment={comment}
+                        isLoading={isLoading}
+                    />
+                ))
+                :<Text text={t('Комментарии отсутствуют')}/>
+            }
+        </div>
+    );
+});
