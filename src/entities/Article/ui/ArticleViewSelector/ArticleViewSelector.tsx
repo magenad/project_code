d84628aa9536be1d58c69b9bcd422031a1ea2351
sadmin <wolf.krsk@gmail.com>
2025-04-29 17:48:13 +0700

    54 add fetching articles, view type selector

---
+++ src/entities/Article/ui/ArticleViewSelector/ArticleViewSelector.tsx
@@ -0,0 +1,50 @@
+import { classNames } from 'shared/lib/classNames/classNames';
+import { useTranslation } from 'react-i18next';
+import cls from './ArticleViewSelector.module.scss';
+import { memo } from 'react';
+import { ArticleView } from '../../model/types/article';
+import ListIcon from 'shared/assets/icons/list-24-24.svg';
+import TiledIcon from 'shared/assets/icons/tiled-24-24.svg';
+import { Button, ThemeButton } from 'shared/ui/Button/Button';
+import { Icon } from 'shared/ui/Icon/Icon';
+
+interface ArticleViewSelectorProps {
+    className?: string;
+    view: ArticleView,
+    onViewClick?: (view: ArticleView) => void
+}
+
+const viewTypes = [
+    {
+        view: ArticleView.SMALL,
+        icon: TiledIcon
+    }, {
+        view: ArticleView.BIG,
+        icon: ListIcon
+    }
+];
+export const ArticleViewSelector = memo((props: ArticleViewSelectorProps) => {
+    const { className, onViewClick, view } = props;
+    const { t } = useTranslation();
+    const onClick = (newView: ArticleView) => () => {
+        onViewClick?.(newView);
+
+    };
+    return (
+        <div className={classNames(cls.ArticleViewSelector, {}, [className])}>
+            {viewTypes.map(viewType => (
+                <Button
+                    theme={ThemeButton.CLEAR}
+                    key={viewType.view}
+                    onClick={onClick(viewType.view)}
+
+                >
+                    <Icon
+                        Svg={viewType.icon}
+                        className={classNames('', { [cls.notSelected]: viewType.view !== view })}
+                    />
+                </Button>
+            ))}
+        </div>
+    );
+});
