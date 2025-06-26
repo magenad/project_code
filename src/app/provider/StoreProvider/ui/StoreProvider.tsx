e5168c07d6d1d8d3432d5f8c5fc2e08297f374d5
sadmin <wolf.krsk@gmail.com>
2025-06-26 12:41:52 +0700

    add circular dependency plugin, refactor LangSwitcher

--- src/app/provider/StoreProvider/ui/StoreProvider.tsx
+++ src/app/provider/StoreProvider/ui/StoreProvider.tsx
@@ -1,7 +1,8 @@
 import { ReactNode } from 'react';
 import { Provider } from 'react-redux';
-import { createReduxStore, StateSchema } from 'app/provider/StoreProvider';
+import { StateSchema } from '../config/StateSchema';
 import { ReducersMapObject } from '@reduxjs/toolkit';
+import { createReduxStore } from '../config/store';
 
 interface StoreProviderProps {
     children?: ReactNode;
--- src/features/AddCommentForm/ui/AddCommentForm/AddCommentForm.async.tsx
+++ src/features/AddCommentForm/ui/AddCommentForm/AddCommentForm.async.tsx
@@ -1,8 +1,6 @@
 import { FC, lazy } from 'react';
 
-import AddCommentForm, { AddCommentFormProps } from './AddCommentForm';
+import { AddCommentFormProps } from './AddCommentForm';
 
 export const AddCommentFormAsync =  lazy<FC<AddCommentFormProps>>(
     () => import('./AddCommentForm'));
-// export const AddCommentFormAsync = _PROJECT__==='storybook' ? AddCommentForm :  lazy<FC<AddCommentFormProps>>(
-//     () => import('./AddCommentForm'));
