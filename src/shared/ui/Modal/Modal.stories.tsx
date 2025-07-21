279fa00ba4ca99fcfa40dd99786efd37bb5dc1a0
sadmin <wolf.krsk@gmail.com>
2025-07-21 15:42:25 +0700

    fix storybook shared/modal

--- src/shared/ui/Modal/Modal.stories.tsx
+++ src/shared/ui/Modal/Modal.stories.tsx
@@ -8,21 +8,24 @@ export default {
     title: 'shared/Modal',
     component: Modal,
     argTypes: {
-        backgroundColor: { control: 'color' },
-    },
+        backgroundColor: { control: 'color' }
+    }
 } as ComponentMeta<typeof Modal>;
 
-const Template: ComponentStory<typeof Modal> = (args) => <Modal {...args} />;
+const Template: ComponentStory<typeof Modal> = (args) =>
+    <div className="app">
+        <Modal {...args} />
+    </div>;
 
 export const Primary = Template.bind({});
 Primary.args = {
     isOpen: true,
-    children:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa distinctio earum excepturi facilis id labore mollitia nostrum quia soluta temporibus!'
+    children: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa distinctio earum excepturi facilis id labore mollitia nostrum quia soluta temporibus!'
 };
 
 export const Dark = Template.bind({});
 Dark.args = {
     isOpen: true,
-    children:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa distinctio earum excepturi facilis id labore mollitia nostrum quia soluta temporibus!'
+    children: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa distinctio earum excepturi facilis id labore mollitia nostrum quia soluta temporibus!'
 };
 Dark.decorators = [ThemeDecorator(Theme.DARK)];
