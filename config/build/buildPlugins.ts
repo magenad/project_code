9e70cfc965ea064933e0f418eb1f00013b4949bd
sadmin <wolf.krsk@gmail.com>
2025-07-22 16:55:16 +0700

    upgrade webpack config, add babel cache

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -5,55 +5,59 @@ import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
 import CopyPlugin from 'copy-webpack-plugin';
-import CircularDependencyPlugin  from 'circular-dependency-plugin';
+import CircularDependencyPlugin from 'circular-dependency-plugin';
 import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
+
 export function buildPlugins({
-    paths,
-    isDev,
-    apiUrl,
-    project
-}: BuildOptions): webpack.WebpackPluginInstance[] {
+                                 paths,
+                                 isDev,
+                                 apiUrl,
+                                 project
+                             }: BuildOptions): webpack.WebpackPluginInstance[] {
     const plugins = [
         new HTMLWebpackPlugin({
-            template: paths.html,
+            template: paths.html
         }),
         new webpack.ProgressPlugin(),
-        new MiniCssExtractPlugin({
-            filename: 'css/[name].[contenthash:8].css',
-            chunkFilename: 'css/[name].[contenthash:8].css',
-        }),
         new webpack.DefinePlugin({
             _IS_DEV__: JSON.stringify(isDev),
             _API__: JSON.stringify(apiUrl),
             _PROJECT__: JSON.stringify(project)
         }),
-        new CopyPlugin({
-            patterns: [
-                { from: paths.locales, to: paths.buildLocales }
-            ],
-        }),
         new CircularDependencyPlugin({
             exclude: /node_modules/,
-            failOnError: true,
+            failOnError: true
         }),
         new ForkTsCheckerWebpackPlugin({
             typescript: {
                 diagnosticOptions: {
                     semantic: true,
-                    syntactic: true,
+                    syntactic: true
                 },
-                mode: 'write-references',
-            },
-        }),
+                mode: 'write-references'
+            }
+        })
     ];
     if (isDev) {
         plugins.push(new webpack.HotModuleReplacementPlugin());
         plugins.push(
             new BundleAnalyzerPlugin({
-                openAnalyzer: false,
+                openAnalyzer: false
             })
         );
         plugins.push(new ReactRefreshWebpackPlugin());
+    } else { //isProd
+        plugins.push(
+            new MiniCssExtractPlugin({
+                filename: 'css/[name].[contenthash:8].css',
+                chunkFilename: 'css/[name].[contenthash:8].css'
+            }),
+            new CopyPlugin({
+                patterns: [
+                    { from: paths.locales, to: paths.buildLocales }
+                ]
+            })
+        );
     }
     return plugins;
 }
