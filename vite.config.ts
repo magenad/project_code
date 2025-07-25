c78ac3e8996cf486f43ddc473b756aa435def1cc
sadmin <wolf.krsk@gmail.com>
2025-07-25 16:33:18 +0700

    fix vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import svgr from 'vite-plugin-svgr';
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [svgr(), react()],
+    plugins: [svgr({ exportAsDefault: true }), react()],
     resolve: {
         alias: [
             { find: '@', replacement: '/src' }]
